942fe789aefe2343dbb432d4293c3ddbe18e2ce0
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2021-04-02 00:22:06 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -11,12 +11,12 @@ class Config(object):
 
     # REQUIRED
     API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR OWNER ID"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_ID = "YOUR OWNER ID" 
     OWNER_USERNAME = "akhilbaiju"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  
+    MESSAGE_DUMP = None 
     LOAD = []
     NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
