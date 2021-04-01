1f87038c91bb00224a0210a04668e95d581d2efa
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2021-04-02 00:23:12 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -22,17 +22,17 @@ class Config(object):
     WEBHOOK = False
     URL = None
 
-    # OPTIONAL
+ 
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  
     WHITELIST_USERS = []  
-    DONATION_LINK = None  # EG, paypal
+    DONATION_LINK = None  
     CERT_PATH = None
     PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    DEL_CMDS = False  
     STRICT_GBAN = False
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    WORKERS = 8  
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg' 
     ALLOW_EXCL = False  # Allow ! commands as well as /
 
 
